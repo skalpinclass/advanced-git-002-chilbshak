2c2dee5dcc8bdc9f2ae44ccbcfedf7831c9f01f7
IdoN <idon@idon-mac.attlocal.net>
2020-04-23 19:02:16 -0400

    Message was changed

--- Magenic.Cli/Program.cs
+++ Magenic.Cli/Program.cs
@@ -6,7 +6,7 @@ namespace Magenic.Cli
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World ... but I'm very tired!");
         }
     }
 }
